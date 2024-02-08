d8c65f7258d02656d39e56a657dba15a97cd4b21
kuypepe <114328282+kuypepe@users.noreply.github.com>
2024-02-09 02:16:16 +0700

    Add files via upload

---
+++ MyWebsite/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/style.css">
+
+    <title>My website</title>
+</head>
+<body>
+    <div class="container" >
+
+    <div class="top1">
+        <div class="top1_child1">
+        <h1>Hi I'm Yem Athinithik (AKA Kuy)</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div class="top1_child2">
+        <img src="image/cat.jpg" alt="cat">
+        </div>
+    </div>
+    <div class="top2">
+        <div class="top2_child1">
+            <img src="" alt="Logo">
+            <h2>The Ursa </h2>
+        </div>
+    </div>
+
+    </div> <!-- end of div container-->
+</body>
+</html>
